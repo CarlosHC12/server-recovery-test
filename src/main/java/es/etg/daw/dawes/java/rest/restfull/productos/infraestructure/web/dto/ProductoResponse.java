b1f8feb81e960779f60c690258726276a631febd
Carlos Hurtado <carloshurtadocortes@gmail.com>
2026-01-30 23:53:06 +0100

    w

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/dto/ProductoResponse.java
+++
@@ -1,7 +0,0 @@
-package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.dto;
-
-import java.time.LocalDateTime;
-
-public record ProductoResponse(int id, String nombre, double precio, LocalDateTime createdAt, int categoria) {
-    
-}
